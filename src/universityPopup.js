e9a98308d2c74059ea9a27a941ecc3c4787de63a
unknown <okorojiebube2@gmail.com>
2022-04-07 05:11:52 -0600

    Add university popup markup

---
+++ src/universityPopup.js
@@ -0,0 +1,114 @@
+import uni from './assets/img/'
+
+const uni = `
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="modal">
+  <div class="popup-container">
+    <div class="story more-story" id="read-stories">
+      <img src=${uni} alt="" />
+      <div class="more-story-desc">
+        <h3>	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga illo exercitationem eaque perspiciatis sint voluptatum deleniti sunt ducimus iusto corporis blanditiis nobis ut placeat, nulla aliquid beatae veritatis, dolores doloremque consequuntur rerum et maxime. Corrupti officiis maiores, facilis in deserunt doloremque architecto quaerat placeat recusandae illum aliquam nobis id perferendis</p>
+        <div class="description cancel">
+          <button type="button">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+           `
+
+const readStories = () => {
+	document.querySelector('.more-stories-popup').innerHTML = stories
+	document.querySelector('.view-more-stories-popup').innerHTML = moreStories
+}
+export default readStories
