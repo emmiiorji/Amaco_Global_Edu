297bb3a745edc81c33e6aaf97d704ebd8a7591b9
emmiiorji <emmanuelorji2013@gmail.com>
2022-05-29 16:37:01 +0100

    Remove universities static markup

--- src/js/modules/universities.controller.js
+++ src/js/modules/universities.controller.js
@@ -14,61 +14,6 @@ const markupCountries = (countriesList) => {
   return markup;
 }
 
-const universities = `
-         <div class="university">
-            <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-					</div>
-        
-          <div class="university">
-            <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-          </div>
-      
-          <div class="university">
-            <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-          </div>
- 
-        
-          <div class="university">
-           <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-          </div>
-  
-     
-          <div class="university">
-           <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-          </div>
-    
-    
-          <div class="university">
-            <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-          </div>
-      
-       
-          <div class="university">
-            <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-          </div>
-   
-    
-          <div class="university">
-            <img src=${uni} alt="university">
-            <h2>Lorem, ipsum dolor.</h2>
-            <p>Lorem, ipsum.</p>
-          </div>
-       
-`
 const renderUniversities = () => {
 	document.querySelectorAll('.university-lists').forEach(uni => {
 		uni.innerHTML = universities
